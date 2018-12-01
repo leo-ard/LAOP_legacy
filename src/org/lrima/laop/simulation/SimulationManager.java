6d0a99ddce77eeeb4f0e456e6f44098b6d8dac5a
lool01 <lool45162@gmail.com>
2018-12-01 12:55:22 -0500

    Initialisation

---
+++ src/org/lrima/laop/simulation/SimulationManager.java
@@ -0,0 +1,8 @@
+package org.lrima.laop.simulation;
+
+public class SimulationManager {
+    private int simulationCount;
+    private SimulationManagerModel simulationManagerModel;
+
+
+}
---
+++ src/org/lrima/laop/simulation/SimulationManagerModel.java
@@ -0,0 +1,6 @@
+package org.lrima.laop.simulation;
+
+public class SimulationManagerModel {
+
+
+}
